345eb007d3d429bf853e3d39aa251578f6c2f92f
Eddy Ramos <eddyinf605@gmail.com>
2021-10-08 19:15:23 -0400

    subiendo el proyecto

---
+++ app.js
@@ -0,0 +1,17 @@
+const caja = document.querySelector('.container');
+const cajaR = caja.getBoundingClientRect();
+
+caja.addEventListener('mousemove', e => {
+    const xP = (e.clientX - cajaR.left) / cajaR.width;
+    const yP = (e.clientY - cajaR.top) / cajaR.height - 0.6;
+    const xOffset = -(xP - 0.6);
+    const dcNorm = Math.min(Math.max(xOffset, -0.6), 0.6);
+    caja.style.transform = `perspective(1000px)
+                            rotateY(${dxNorm*45}deg)
+                            rotateX(${yP*45}deg)`;
+
+});
+
+caja.addEventListener('mouseleave', () => {
+    caja.style.transform = 'none';
+})
